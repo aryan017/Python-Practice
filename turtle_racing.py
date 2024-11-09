ed0bf0a3b18e8b981a45d41538e46adbce7aa14b
aryan017 <68499358+aryan017@users.noreply.github.com>
2024-11-09 20:41:21 +0530

    Turtle racing part 2

--- turtle_racing.py
+++ turtle_racing.py
@@ -4,8 +4,20 @@ import random
 
 WIDTH,HEIGHT=500,500
 
-COLORS=['black','white','red','green','blue','yellow','cyan','magenta','gray','brown','orange','purple','pink']
+COLORS=['black','red','green','blue','yellow','cyan','magenta','gray','brown','orange','purple','pink']
 
+def start_Race(colors):
+    All_Turtles=create_Racing_Turtles(colors)
+    while True:
+        for winner_turtle in All_Turtles:
+            winner_turtle.forward(random.randrange(1,20))
+        
+            x,y=winner_turtle.pos()
+        
+            if y>= (HEIGHT//2) -10:
+                return colors[All_Turtles.index(winner_turtle)]
+            
+    
 def get_Number_of_Racing_Turtles():
     
     racers=0
@@ -23,14 +35,17 @@ def get_Number_of_Racing_Turtles():
             
 def create_Racing_Turtles(colors):
     turtles=[]
+    spacing_between_turtles=WIDTH//(len(colors)+1)
     for i, color in enumerate(colors):
         Racing_Turtle=turtle.Turtle()
         Racing_Turtle.color(color)
         Racing_Turtle.shape('turtle')
         Racing_Turtle.left(90)
         Racing_Turtle.penup()
+        Racing_Turtle.setposition((-WIDTH//2+((i+1)*spacing_between_turtles)),-HEIGHT//2+20)
         Racing_Turtle.pendown()
         turtles.append(Racing_Turtle)
+    return turtles
     
 racers=get_Number_of_Racing_Turtles()
 
@@ -44,4 +59,6 @@ init_Turtle_Racing_Window()
 random.shuffle(COLORS)
 colors=COLORS[:racers]
 
-create_Racing_Turtles(colors)
+Winner_Turtle_Name=start_Race(colors)
+
+print("So the Winner is "+Winner_Turtle_Name+" Turtle")
